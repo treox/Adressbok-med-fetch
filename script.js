94969e99499443481f1dcd4d15300ad4a1035b01
Tero Viitala <vite19ax@student.ju.se>
2019-09-27 21:37:51 +0200

    First commit

---
+++ script.js
@@ -0,0 +1,35 @@
+// Ge åtkomst till element:
+var body1 = document.getElementById('body-1');
+
+// fetch som hämtar JSON data:
+fetch('adressbok-1.json')
+    .then(function(response) {
+        return response.json();
+    })
+    .then(function(adressbok) {
+        console.log(adressbok);
+
+        // Lägger till <ul> och ger åtkomst till <ul>:
+        body1.insertAdjacentHTML('afterbegin', '<ul id="lista-1">Lista</ul>');
+        var lista1 = document.getElementById('lista-1');
+
+        // Sorterar JSON data efter ålder:
+        adressbok.sort(function (a, b) {
+            return a.age - b.age;
+          });
+        
+        console.log(adressbok);
+
+        // Lägger till sorterad JSON data i <ul>:
+        liElement = "";
+        for (i=0; i<adressbok.length; i++) {
+            liElement += "<li> <span class=\"sp\"> Förnamn: </span> " + adressbok[i].firstname + " <span class=\"sp\"> Efternamn: </span> " + adressbok[i].lastname + " <span class=\"sp\"> Epost: </span> " + adressbok[i].email + " </li>";
+        } // Slut på loop
+        lista1.innerHTML = liElement;
+    })
+    // Catch funktion som fångar upp fel om data ite laddas korrekt:
+    .catch(function(err) {
+        console.log(JSON.stringify(err));
+    });
+
+
